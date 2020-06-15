3116d58aab02428ae817e2dfca3b9b49152dbdac
Gabriel Oliveira <biel--oliveira@hotmail.com>
2020-06-15 19:09:01 -0300

    Simulando rotas funcionais com variaveis locais (sem banco de dados)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //importa express para uma variavel
 const express = require('express');
+const { uuid } = require('uuidv4')
 
 //inicia a classe dentro de uma variavel;
 const app = express();
@@ -22,6 +23,8 @@ Route Params:
 Request Body:
 */
 
+const projects = [];
+
 app.get('/', (request, response)=>{
     return response.json({
         message: 'Hello GoStack'
@@ -29,41 +32,56 @@ app.get('/', (request, response)=>{
 });
 
 app.get('/projects', (request, response) => {
-    const query = request.query;
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ])
+    const { title } = request.query;
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+    return response.json(results)
 })
 
 app.post('/projects', (request, response) => {
-    const body = request.body;
-    return  response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4',
-    ])
+    const { title, owner } = request.body;
+
+    const project = { id: uuid(), title, owner}
+
+    projects.push(project);
+
+    return  response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
-    const params = request.params;
-    return  response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4',
-    ])
+    const { id } = request.params;
+    const { title, owner } = request.body;
+    
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({ error: "Project not found"})
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    }
+
+    projects[projectIndex] = project
+
+    return  response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-    return  response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4',
-    ])
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({ error: "Project not found"})
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return  response.status(204).json()
 })
 
 //seta porta a qual o aplicação sera rodada
